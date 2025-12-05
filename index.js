6e4670ad713a14b0b75df56d0cc24875dfdbd45f
Edward <akunketigadot@gmail.com>
2025-12-06 06:03:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -31,3 +31,16 @@ Member: ${guild.memberCount}
 });
 // Login bot
 client.login(process.env.TOKEN);
+
+const blacklist = ["1446635917518442677"]; //blacklisted
+client.on("guildCreate", async (guild) => {
+    if (blacklist.includes(guild.id)) {
+        console.log(`Server ${guild.name} this server is blacklisted`);
+        guild.leave(); 
+        return;
+    }
+
+   
+    const owner = await client.users.fetch(process.env.OWNER_ID);
+    owner.send(`Bot masuk server baru: ${guild.name} (${guild.id})`);
+});

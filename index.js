2149229210e390bf0e4a9ad986b72d8c9378aa23
Edward <akunketigadot@gmail.com>
2025-12-06 06:06:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -32,15 +32,25 @@ Member: ${guild.memberCount}
 // Login bot
 client.login(process.env.TOKEN);
 
-const blacklist = ["1446635917518442677"]; //blacklisted
+const blacklist = ["1446635917518442677"]; // blacklisted
+
 client.on("guildCreate", async (guild) => {
     if (blacklist.includes(guild.id)) {
-        console.log(`Server ${guild.name} this server is blacklisted`);
-        guild.leave(); 
+        console.log(`Server ${guild.name} is blacklisted. bot will leave.`);
+        try {
+            await guild.leave(); 
+            console.log(`bot successfully leave: ${guild.name}`);
+        } catch (err) {
+            console.error("failure:", err);
+        }
         return;
     }
 
-   
-    const owner = await client.users.fetch(process.env.OWNER_ID);
-    owner.send(`Bot masuk server baru: ${guild.name} (${guild.id})`);
+
+    try {
+        const owner = await client.users.fetch(process.env.OWNER_ID);
+        await owner.send(`bot entered: ${guild.name} (${guild.id})`);
+    } catch (err) {
+        console.error("failed:", err);
+    }
 });
